763abef8f26081c340fb8f70ac64cad6ce4091a7
awaresj11 <awaresj11@gmail.com>
2026-07-23 13:00:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/api/status")
 def api_status():
-  return jsonify(
-    {
-      "status": "success",
-      "message": "Server is up & running"
+    return jsonify({
+        "status": "success",
+        "message": "Server is up & running"
     })
+
 
 @app.route("/health")
 def health():
-  return (
-    jsonify(
-        {
-           "status": "UP"                                                                           }),
-    200 ,)
-if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000, debug=True)
+    return jsonify({
+        "status": "UP"
+    }), 200
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
